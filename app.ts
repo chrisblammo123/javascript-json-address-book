3abd00962cc758df46d0cdd3c69be88a91c5933c
Chris Struck <chris.struck@cstruck.dev>
2026-05-21 16:42:27 -0500

    changes from laptop

---
+++ app.ts
@@ -0,0 +1,20 @@
+// includes here (tba)
+
+// declare type date
+
+
+
+console.log('Initialization complete', Temporal.Now.instant());			// requires node 26 i think?
+
+interface Person {
+	name: {
+		prefix: '',
+		first_name: '',
+		middle_name: '',
+		last_name: '',
+		suffix: '',
+		nickname: '',
+		former_name: ''
+	};
+	// birthday: date
+};
